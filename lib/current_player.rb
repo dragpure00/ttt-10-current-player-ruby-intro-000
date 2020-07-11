22de1d8dd5c65d6da0020b7eecf44303a75afe15
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-11 01:06:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@ def turn_count(board)
   board.each do |c|
     if c == "X"||c == "0"
       turn+=1
-  end 
+  end
 end
 return turn
 end
@@ -14,9 +14,9 @@ end
 def current_player(board)
   turn = turn_count(board)
   if(turn %2 ==0)
-    
+
     return "X"
   else
     return "O"
-  
+
 end
